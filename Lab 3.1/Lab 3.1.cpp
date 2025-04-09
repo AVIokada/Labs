88947a276efdd188a513001b3f28d4676d87c668
AVIokada <155766369+AVIokada@users.noreply.github.com>
2025-04-09 11:23:13 +0300

    3

---
+++ Lab 3.1/Lab 3.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL(), "ru");
+
+    int a = 11;
+    int* b = &a;
+    cout << "a = " << a << endl;
+    cout << "указатель а = " << b << endl;
+    
+    *b = 15;
+    cout << "a = " << a << endl;
+    cout << "указатель а = " << b << endl;
+
+    int arr[3] = { 101, 102, 103 };
+    int* m = arr;
+    cout << "Значения массива: ";
+    for (int i = 0; i < 3; i++) 
+    {
+        cout << *(m+i) << " ";
+    }
+    cout << endl;
+
+    int x = 5;
+    int* const y = &x;
+    cout << x << " = " << y << endl;
+    //y++;
+    *y = x++;
+    cout << x << " = " << y << endl;
+}
---
+++ Lab 3.2/Lab 3.2.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+int s(int* begin, int* end) {
+    int c = 0;
+    for (int* i = begin; i < end; i++) {
+        c += *i;
+    }
+    return c;
+}
+
+
+int add(int a, int b) {
+    return a + b;
+}
+
+
+int minu(int a, int b) {
+    return a - b;
+}
+
+
+int (*znak(char z))(int, int) {
+    if (z == '+') { return add; }
+    else { return minu; }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "ru");
+
+    int k[5] = {5, 4, 3, 2, 1};
+    cout << s(begin(k), end(k)) << endl;
+    
+    int (*operation) (int, int);
+    operation = znak('+');
+
+    //auto operation = znak('+');
+
+    int x = operation(4, 5);
+    cout << x << " ";
+
+    operation = znak('-');
+    int y = operation(11, 4);
+    cout << y << endl;
+
+    float n = 12.34;
+    float* m = new float(n);
+    cout << *m << " = " << m << endl;
+    delete m;
+}
